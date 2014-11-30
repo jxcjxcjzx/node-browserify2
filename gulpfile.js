ceea447a95b7768eebe36b83001cedabbbe84c5e
Junle Li <lijunle@gmail.com>
2014-11-30 22:46:50 +0800

    Add a new task to generate expose index.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,31 @@
 'use strict';
 
 var gulp = require('gulp');
+var _ = require('lodash');
 var browserify = require('browserify');
 var reactify = require('reactify');
+var File = require('vinyl');
 var source = require('vinyl-source-stream');
+
+var dest = gulp.dest('./build/');
+
+var files = {
+  'test1': './test1.jsx',
+  'test2': './test2.jsx'
+};
+
+gulp.task('index', function () {
+  var indexContent = _.map(files, function (file) {
+    return 'require(\'' + file + '\');';
+  }).join('\n');
+
+  var indexFile = new File({
+    contents: new Buffer(indexContent)
+  });
+
+  indexFile.pipe(source('index.js'))
+    .pipe(dest);
+});
 
 gulp.task('default', function () {
   var bd = browserify({
@@ -16,5 +38,5 @@ gulp.task('default', function () {
   bd.transform(reactify)
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./build/'));
+    .pipe(dest);
 });
